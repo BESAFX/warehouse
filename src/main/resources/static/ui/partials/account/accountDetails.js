49f68a40d0b664d8b230153e80b6eee4060fe301
Development <islamhaker@gmail.com>
2017-09-30 18:34:10 +0300

    NEVER

--- src/main/resources/static/ui/partials/account/accountDetails.js
+++ src/main/resources/static/ui/partials/account/accountDetails.js
@@ -114,7 +114,6 @@ app.controller('accountDetailsCtrl', ['AccountConditionService', 'AccountNoteSer
             angular.forEach(files, function (file) {
                 var wrapper = {};
                 wrapper.src = file;
-                console.info(file);
                 wrapper.name = file.name;
                 wrappers.push(wrapper);
             });
@@ -195,6 +194,7 @@ app.controller('accountDetailsCtrl', ['AccountConditionService', 'AccountNoteSer
             for(var i = 0; (scannedImages instanceof Array) && i < scannedImages.length; i++) {
                 var wrapper = {};
                 wrapper.src = scannedImages[i].src;
+                console.info(wrapper.src);
                 wrappers.push(wrapper);
             }
 
