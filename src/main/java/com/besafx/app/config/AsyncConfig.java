225c96d185b50b03b99c7780df953587dd108890
Development <islamhaker@gmail.com>
2018-01-03 12:48:52 +0300

    NEVER

--- src/main/java/com/besafx/app/config/AsyncConfig.java
+++ src/main/java/com/besafx/app/config/AsyncConfig.java
@@ -14,11 +14,11 @@ public class AsyncConfig {
 
     private final Logger log = LoggerFactory.getLogger(AsyncConfig.class);
 
-    @Bean(name = "threadPoolEmailSender")
-    public Executor threadPoolTaskExecutor() {
-        log.info("Prepare threadPoolEmailSender...");
-        return initThreadPool(1, 1, 500, "EmailSender-");
-    }
+//    @Bean(name = "threadPoolEmailSender")
+//    public Executor threadPoolTaskExecutor() {
+//        log.info("Prepare threadPoolEmailSender...");
+//        return initThreadPool(1, 1, 500, "EmailSender-");
+//    }
 
 //    @Bean(name = "threadPoolTwilioSMS")
 //    public Executor threadPoolTwilioSMS() {
@@ -32,35 +32,35 @@ public class AsyncConfig {
 //        return initThreadPool(5, 10, 500, "BulkSMS-");
 //    }
 
-    @Bean(name = "threadPoolFileUploader")
-    public Executor threadPoolFileUploader() {
-        log.info("Prepare threadPoolFileUploader...");
-        return initThreadPool(5, 10, 500, "FileUploader-");
-    }
-
-    @Bean(name = "threadPoolFileSharing")
-    public Executor threadPoolFileSharing() {
-        log.info("Prepare threadPoolFileSharing...");
-        return initThreadPool(5, 10, 500, "FileSharing-");
-    }
-
-    @Bean(name = "threadPoolReportGenerator")
-    public Executor threadPoolReportGenerator() {
-        log.info("Prepare threadPoolReportGenerator...");
-        return initThreadPool(1, 1, 500, "FilePDFGenerate-");
-    }
-
-    @Bean(name = "threadPoolPaymentExcel")
-    public Executor threadPoolPaymentExcel() {
-        log.info("Prepare threadPoolPaymentExcel...");
-        return initThreadPool(1, 1, 500, "PaymentExcel-");
-    }
-
-    @Bean(name = "JasperPrintGenerator")
-    public Executor JasperPrintGenerator() {
-        log.info("Prepare JasperPrintGenerator...");
-        return initThreadPool(10, 10, 500, "JasperPrintGenerator-");
-    }
+//    @Bean(name = "threadPoolFileUploader")
+//    public Executor threadPoolFileUploader() {
+//        log.info("Prepare threadPoolFileUploader...");
+//        return initThreadPool(5, 10, 500, "FileUploader-");
+//    }
+//
+//    @Bean(name = "threadPoolFileSharing")
+//    public Executor threadPoolFileSharing() {
+//        log.info("Prepare threadPoolFileSharing...");
+//        return initThreadPool(5, 10, 500, "FileSharing-");
+//    }
+//
+//    @Bean(name = "threadPoolReportGenerator")
+//    public Executor threadPoolReportGenerator() {
+//        log.info("Prepare threadPoolReportGenerator...");
+//        return initThreadPool(1, 1, 500, "FilePDFGenerate-");
+//    }
+//
+//    @Bean(name = "threadPoolPaymentExcel")
+//    public Executor threadPoolPaymentExcel() {
+//        log.info("Prepare threadPoolPaymentExcel...");
+//        return initThreadPool(1, 1, 500, "PaymentExcel-");
+//    }
+//
+//    @Bean(name = "JasperPrintGenerator")
+//    public Executor JasperPrintGenerator() {
+//        log.info("Prepare JasperPrintGenerator...");
+//        return initThreadPool(10, 10, 500, "JasperPrintGenerator-");
+//    }
 
     private ThreadPoolTaskExecutor initThreadPool(int corePoolSize, int maxPoolSize, int queueCapacity, String prefix) {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
